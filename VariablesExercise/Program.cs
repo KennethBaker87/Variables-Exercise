8334383769172e80657d6f4bdbf196af4335eacf
Kenneth Baker <spyksaturn@gmail.com>
2023-01-30 16:07:27 -0500

    first commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number1 = 1;
+            double number2 = 2;
+            decimal number3 = 3.4m;
+            bool yes = true;
+            char ah = 'a';
+            string bird = "Bird is the word";
+
+            Console.WriteLine($"{number1}, {number2}, {number1} {number2} {number3} ");
+            Console.WriteLine($"{ah} ba ba bird bird bird");
+            Console.WriteLine($"{bird}");
         }
     }
 }
